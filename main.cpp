673c10737a010eba3115187135c1cfcc9b5a41d6
Kevin Napoli <kvnnap@gmail.com>
2018-05-26 13:46:22 +0200

    Basic computation on GPU

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 void checkStatus(cl_int status, string message) {
     if (status != CL_SUCCESS) {
-        throw runtime_error(message);
+        throw runtime_error(message + ". Status: " + to_string(status));
     }
 }
 
@@ -78,6 +78,16 @@ void printDeviceInfo(const vector<cl_device_id >& deviceIds) {
             cout << "Device Max Work Item Dimensions: " << number << endl;
         }
 
+        size_t sizes[number];
+        status = clGetDeviceInfo(id, CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(sizes), &sizes, &buffSizeWritten);
+        if (status == CL_SUCCESS) {
+            cout << "Device Max Work Item Sizes: (";
+            for (size_t i = 0; i < number; ++i) {
+                cout << sizes[i] << ", ";
+            }
+            cout << ")" << endl;
+        }
+
         status = clGetDeviceInfo(id, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(number), &number, &buffSizeWritten);
         if (status == CL_SUCCESS) {
             cout << "Device Max Clock Frequency: " << number << " MHz" << endl;
@@ -100,11 +110,27 @@ size_t getUserNumericInput(string message) {
     return result;
 }
 
+//OpenCL kernel which is run for every work item created.
+const char *saxpy_kernel =
+        "__kernel                                   \n"
+        "void saxpy_kernel(float alpha,             \n"
+        "                  __global float *A,       \n"
+        "                  __global float *B,       \n"
+        "                  __global float *C        \n"
+//        "                  uint maxIterations       \n"
+        "                                    )      \n"
+        "{                                          \n"
+        "    //Get the index of the work-item       \n"
+        "    int index = get_global_id(0);          \n"
+//        "    uint i;                                 \n"
+//        "    for(i = 0; i < maxIterations; ++i) {   \n"
+        "        C[index] = alpha* A[index] + B[index]; \n"
+//        "    }                                      \n"
+        "}                                          \n";
+
 int main() {
 
     try {
-        vector<float> a, b, c;
-
         // Generic return value
         cl_int status;
 
@@ -128,7 +154,7 @@ int main() {
         // Query all devices available
         size_t platformNumber = platformIds.size() > 1 ? getUserNumericInput("Which platform would you like to use?") : 0;
         cl_platform_id platformId = platformIds.at(platformNumber);
-        cout << "Selected platform id: " << platformId << endl;
+        cout << "Selected platform id: " << platformId << endl << endl;
 
         // Get the device list in this platform
         cl_uint num_devices;
@@ -140,8 +166,106 @@ int main() {
         status = clGetDeviceIDs(platformId, CL_DEVICE_TYPE_ALL, num_devices, deviceIds.data(), nullptr);
         printDeviceInfo(deviceIds);
 
-        //
+        // Select device to use
+        size_t deviceNumber = deviceIds.size() > 1 ? getUserNumericInput("Which device would you like to use?") : 0;
+        cl_device_id deviceId = deviceIds.at(deviceNumber);
+        cout << "Selected device id: " << deviceId << endl << endl;
 
+        // Create context - takes a list of devices but I'm only interested in one
+        cl_context context = clCreateContext(nullptr, 1, &deviceId, nullptr, nullptr, &status);
+        checkStatus(status, "Cannot create context");
+
+        // Create a command queue to send commands to the device
+        // A command queue maps only with one device
+        cl_command_queue commandQueue = clCreateCommandQueueWithProperties(context, deviceId, nullptr, &status);
+        checkStatus(status, "Cannot create command queue");
+
+        // Allocate data on host memory
+        const size_t vecSize = 268435456 / 1024;
+//        const uint32_t maxIterations = 1024;
+        vector<float> a (vecSize, 1.f);
+        vector<float> b (vecSize, 2.f);
+        vector<float> c (vecSize, 0.f);
+
+        // Create buffer on device memory with the same size as the host buffer
+        cl_mem devBuffA = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * a.size(), nullptr, &status);
+        checkStatus(status, "Cannot allocate memory for vector A");
+        cl_mem devBuffB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * b.size(), nullptr, &status);
+        checkStatus(status, "Cannot allocate memory for vector B");
+        cl_mem devBuffC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * c.size(), nullptr, &status);
+        checkStatus(status, "Cannot allocate memory for vector C");
+
+        // Enqueue a copy command to copy from host to device memory
+        status = clEnqueueWriteBuffer(commandQueue, devBuffA, CL_TRUE, 0, sizeof(float) * a.size(), a.data(), 0, nullptr, nullptr);
+        checkStatus(status, "Cannot copy memory for vector A");
+        status = clEnqueueWriteBuffer(commandQueue, devBuffB, CL_TRUE, 0, sizeof(float) * b.size(), b.data(), 0, nullptr, nullptr);
+        checkStatus(status, "Cannot copy memory for vector B");
+
+        // Create a program
+        cl_program program = clCreateProgramWithSource(context, 1, &saxpy_kernel, nullptr, &status);
+        checkStatus(status, "Cannot create program with source");
+
+        // Build it
+        status = clBuildProgram(program, 1, &deviceId, nullptr, nullptr, nullptr);
+        checkStatus(status, "Cannot build program");
+
+        // Create/Get the kernel from our program
+        cl_kernel kernel = clCreateKernel(program, "saxpy_kernel", &status);
+        checkStatus(status, "Cannot create kernel from program");
+
+        // Set the arguments of the kernel
+        const float alpha = 25.f;
+        status = clSetKernelArg(kernel, 0, sizeof(float), &alpha);
+        checkStatus(status, "Cannot set argument 0 of kernel");
+        status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &devBuffA);
+        checkStatus(status, "Cannot set argument 1 of kernel");
+        status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &devBuffB);
+        checkStatus(status, "Cannot set argument 2 of kernel");
+        status = clSetKernelArg(kernel, 3, sizeof(cl_mem), &devBuffC);
+        checkStatus(status, "Cannot set argument 3 of kernel");
+//        status = clSetKernelArg(kernel, 4, sizeof(uint32_t), &maxIterations);
+//        checkStatus(status, "Cannot set argument 4 of kernel");
+
+        // Execute the kernel :D
+        size_t globalSizeOfItems = c.size();
+        size_t blockSize = 128;
+        status = clEnqueueNDRangeKernel(commandQueue, kernel, 1, nullptr, &globalSizeOfItems, &blockSize, 0,
+                                        nullptr, nullptr);
+        checkStatus(status, "Could not execute the kernel");
+
+        // Read result from device memory
+        status = clEnqueueReadBuffer(commandQueue, devBuffC, CL_TRUE, 0, sizeof(float) * c.size(), c.data(), 0,
+                                     nullptr, nullptr);
+        checkStatus(status, "Could not read the device buffer for the result");
+
+        // Flush the command queue - send commands to device
+        status = clFlush(commandQueue);
+        checkStatus(status, "Could not flush the command queue");
+
+        // Finish waits for the device to process the commands
+        status = clFinish(commandQueue);
+        checkStatus(status, "Could not finish on the command queue");
+
+        // Here we can show the processed list
+        cout << "One item from result: " << c.at(0) << endl;
+
+        // Release everything
+        status = clReleaseKernel(kernel);
+        checkStatus(status, "Cannot release kernel");
+        status = clReleaseProgram(program);
+        checkStatus(status, "Cannot release program");
+        status = clReleaseMemObject(devBuffA);
+        checkStatus(status, "Cannot allocate memory for vector A");
+        status = clReleaseMemObject(devBuffB);
+        checkStatus(status, "Cannot allocate memory for vector B");
+        status = clReleaseMemObject(devBuffC);
+        checkStatus(status, "Cannot allocate memory for vector C");
+
+        status = clReleaseCommandQueue(commandQueue);
+        checkStatus(status, "Cannot release command queue");
+
+        status = clReleaseContext(context);
+        checkStatus(status, "Cannot release context");
     } catch (const exception& e) {
         cout << e.what() << endl;
         return EXIT_FAILURE;
